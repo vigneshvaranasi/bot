8aaca7cd8afd106d0762a6a9209c555b110bb03e
pavancos <pavankc005@gmail.com>
2025-08-08 15:59:33 +0530

    feat: MAJOR added Summariser Agent which summarises the Thread if provided.
    modified Task of Responder to take in account of context while responding

--- src/support_bot/crew.py
+++ src/support_bot/crew.py
@@ -1,10 +1,17 @@
 from crewai import Crew, Process
-from .tasks import research_task, synthesis_task, report_task, user_query_response_task
-from .agents import researcher_agent, synthesizer_agent, expert_writer_agent, user_query_responder_agent
+from .tasks import research_task, synthesis_task, report_task, user_query_response_task, json_summary_task
+from .agents import researcher_agent, synthesizer_agent, expert_writer_agent, user_query_responder_agent, json_summary_agent
 
 support_crew = Crew(
     agents=[researcher_agent, synthesizer_agent, expert_writer_agent, user_query_responder_agent],
     tasks=[research_task, synthesis_task, report_task, user_query_response_task],
+    process=Process.sequential,
+    verbose=True
+)
+
+conversation_summary_crew = Crew(
+    agents=[json_summary_agent],
+    tasks=[json_summary_task],
     process=Process.sequential,
     verbose=True
 )
