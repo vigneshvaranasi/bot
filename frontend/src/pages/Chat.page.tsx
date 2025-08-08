82a23c47c4dfc065a5d0c32e859fd2e2ed100a6e
pavancos <pavankc005@gmail.com>
2025-08-08 13:54:26 +0530

    refactor: used Button in Chat Page.
    modified Bubble to gray to adapt background.

--- frontend/src/pages/Chat.page.tsx
+++ frontend/src/pages/Chat.page.tsx
@@ -1,10 +1,21 @@
+import { useRef } from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
+import { Button } from '../components/ui/Button'
 import { useSidebarContext } from '../hooks/useSidebarContext'
 import { Outlet } from 'react-router-dom'
 
 function ChatPage () {
-  const { isSidebarOpen } = useSidebarContext()
+  const { isSidebarOpen } = useSidebarContext();
+  const promptInputRef = useRef<HTMLInputElement>(null);
+
+  const handlePromptSend = ()=>{
+    const prompt = promptInputRef.current?.value;
+    if(prompt){
+      console.log('Sent a Prompt:\n', prompt);
+    }
+  }
+
   return (
     <div className={`flex h-screen`}>
       <Sidebar />
@@ -12,20 +23,22 @@ function ChatPage () {
         <div className='flex flex-col h-screen'>
           <Navbar />
           <Outlet />
-          {/* Input Box */}
+          {/* Prompt Box */}
           <div
-            className='flex p-5 border-t border-gray-200 bg-gray-50 w-full'
+            className='flex p-5 gap-x-2 border-t border-gray-200 bg-gray-50 w-full'
           >
             <input
               type='text'
               placeholder='Type your message...'
               className={`w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
+              ref={promptInputRef}
             />
-            <button
-              className={`ml-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500`}
+            <Button
+              variant='secondary'
+              onClick={handlePromptSend}
             >
               Send
-            </button>
+            </Button>
           </div>
         </div>
       </div>
