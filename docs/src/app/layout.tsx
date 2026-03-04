310a3abe7e942479e593c1ad6d05cb4fde11c7c3
vigneshvaranasi <varanasivsv@gmail.com>
2026-03-04 15:26:08 +0000

    docs: add Introduction & Quickstart

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -15,7 +15,9 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen" suppressHydrationWarning>
-        <RootProvider>{children}</RootProvider>
+        <RootProvider theme={{ defaultTheme: 'dark', enableSystem: true }}>
+          {children}
+        </RootProvider>
       </body>
     </html>
   );
